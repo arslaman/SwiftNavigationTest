c3fadf8cfc2fc06778e7e0ee9bea2cc3f09ab01e
Ruslan Mansurov <mansurov.master@gmail.com>
2024-02-21 11:06:14 -0800

    Initial Commit

---
+++ SwiftNavigationTest/SwiftNavigationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNavigationTestApp.swift
+//  SwiftNavigationTest
+//
+//  Created by Ruslan Mansurov on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNavigationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
